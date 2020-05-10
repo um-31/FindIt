d1d2fd21e60e8b0f1e09ed699583d183d064cfe2
um-31 <udaymahajan97@gmail.com>
2020-05-10 18:52:24 -0400

    create card class

---
+++ FindIt/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  FindIt
+//
+//  Created by Udhay on 2020-05-10.
+//  Copyright © 2020 Udhay. All rights reserved.
+//
+
+import Foundation
+
+class Card
+{
+    var imageName = ""
+    var isFlipped = false
+    var isMatched = false
+}
