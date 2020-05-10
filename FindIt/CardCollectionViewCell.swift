7ee9f4e6d645022c531ed417ae7375d06e1c1ddd
um-31 <udaymahajan97@gmail.com>
2020-05-10 18:58:34 -0400

    Card Flip

--- FindIt/CardCollectionViewCell.swift
+++ FindIt/CardCollectionViewCell.swift
@@ -10,4 +10,50 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+    var card:Card?
+    
+    func setCard(_ card:Card)
+    {
+        self.card = card
+        
+        backImageView.image = UIImage.init(named: card.imageName)
+        
+        
+        if card.isMatched
+        {
+            
+            
+            frontImageView.alpha = 0
+            backImageView.alpha = 0
+            
+            return
+        }
+        else{
+            
+            frontImageView.alpha = 1
+            backImageView.alpha = 1
+        }
+        
+        if card.isFlipped
+        {
+            //filp the card
+            UIView.transition(from: frontImageView, to: backImageView, duration: 0, options: [.transitionFlipFromRight,.showHideTransitionViews], completion: nil)
+        }
+        else
+        {
+            UIView.transition(from: backImageView, to: frontImageView, duration: 0, options: [.transitionFlipFromRight,.showHideTransitionViews], completion: nil)
+        }
+    }
+    
+    func flip()
+    {
+        UIView.transition(from: frontImageView, to: backImageView, duration: 0.3, options: [.transitionFlipFromRight,.showHideTransitionViews], completion: nil)
+        
+        card?.isFlipped = true
+    }
+    
 }
--- FindIt/CardModel.swift
+++ FindIt/CardModel.swift
@@ -10,4 +10,37 @@ import Foundation
 
 class CardModel
 {
+    func getCards() -> [Card]
+    {
+        var generatedCardArray = [Card]()
+        var generatedRandomNumbers = [Int]()
+        while generatedRandomNumbers.count < 8
+        {
+            
+            let randomnumber = arc4random_uniform(13) + 1
+            
+            if !generatedRandomNumbers.contains(Int(randomnumber))
+            {
+            
+                
+                generatedRandomNumbers.append(Int(randomnumber))
+                
+                
+                print(randomnumber)
+                
+                
+                let cardOne = Card()
+                cardOne.imageName = "card\(randomnumber)"
+                generatedCardArray.append(cardOne)
+                
+                
+                let cardTwo = Card()
+                cardTwo.imageName = "card\(randomnumber)"
+                generatedCardArray.append(cardTwo)
+            }
+            
+        }
+        generatedCardArray.shuffle()
+        return generatedCardArray
+    }
 }
