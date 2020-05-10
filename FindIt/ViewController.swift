55705747f58f4ba59ef1f13a49571eece757020a
um-31 <udaymahajan97@gmail.com>
2020-05-10 18:57:30 -0400

    Card set

--- FindIt/ViewController.swift
+++ FindIt/ViewController.swift
@@ -11,8 +11,23 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionview: UICollectionView!
+    
+    var model = CardModel()
+    var cardArray = [Card]()
+    var count_flip = 0;
+    
+    var firstFlippedCardIndex:IndexPath?
+    
+    var timer:Timer?
+    var millisecond:Float = 30 * 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cardArray = model.getCards()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
